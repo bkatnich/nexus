0a9e9562ac4053fc71b41cd3e90ed491294a957d
Britton Katnich <katnich@shaw.ca>
2019-02-27 21:49:17 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(require('./api'));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to nexus!');
+  res.send('Welcome to the latest version of nexus!');
 });
 
 // App: Start Listening
